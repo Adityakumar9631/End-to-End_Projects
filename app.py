77856c00f800a70744642e27a10c91634f46c7c7
ADITYA KUMAR <adityakrgolu7488@gmail.com>
2026-03-21 22:03:02 +0530

    updated Index and flask with column name

--- app.py
+++ app.py
@@ -14,22 +14,30 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        year = int(request.form['year'])
-        km_driven = int(request.form['km_driven'])
-        fuel = int(request.form['fuel'])
-        transmission = int(request.form['transmission'])
-        owner = int(request.form['owner'])
-        mileage = float(request.form['mileage'])
-        engine = int(request.form['engine'])
-        power = float(request.form['power'])
-        seats = int(request.form['seats'])
+        year = int(request.form['Year'])
+        km_driven = int(request.form['Kilometers_Driven'])
+        fuel = request.form['Fuel_Type']
+        transmission = request.form['Transmission']
+        owner = request.form['Owner_Type']
+        mileage = float(request.form['Mileage'])
+        engine = float(request.form['Engine'])
+        power = float(request.form['Power'])
+        seats = int(request.form['Seats'])
+
+        fuel_map = {'Petrol': 0, 'Diesel': 1, 'CNG': 2, 'LPG': 3}
+        transmission_map = {'Manual': 0, 'Automatic': 1}
+        owner_map = {'First': 0, 'Second': 1, 'Third': 2, 'Fourth & Above': 3}
+
+        fuel = fuel_map.get(fuel, 0)
+        transmission = transmission_map.get(transmission, 0)
+        owner = owner_map.get(owner, 0)
 
         features = np.array([[year, km_driven, fuel, transmission, owner,
                               mileage, engine, power, seats]])
 
         prediction = model.predict(features)[0]
 
-        return render_template("result.html", prediction=round(prediction, 2))
+        return render_template('result.html', prediction=round(prediction, 2))
 
     except Exception as e:
         return str(e)
